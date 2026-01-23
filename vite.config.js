15c32f52ef2b222dfd72cfb758c58ae1722bd782
Rishi Mourya <rishi.morya@novasarc.com>
2026-01-24 00:56:28 +0530

    Update portfolio

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/rishi-mourya-portfolio/',
+base: '/rishi-portfolio/', 
   server: {
     port: 3000,
     open: true
